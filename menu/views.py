0cf940f8531f89e8c1917cb83d86cd2eaa87c775
Ramadhani09 <148738659+Ramadhani09@users.noreply.github.com>
2023-10-23 10:59:39 +0700

    upload tugas web django

---
+++ menu/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def home(request):
+    home = loader.get_template('home.html')
+    return HttpResponse(home.render())
+
+def produk(request):
+    produk = loader.get_template('produk.html')
+    return HttpResponse(produk.render())
+
+def kategori(request):
+    kategori = loader.get_template('kategori.html')
+    return HttpResponse(kategori.render())
